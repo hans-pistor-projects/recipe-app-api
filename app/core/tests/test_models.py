876f58e046a7e376091ca6ef0384511b3ffd2d3b
Hans <hpistor@stetson.edu>
2019-03-01 21:32:01 -0500

    Addedd recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -62,3 +62,14 @@ class ModelTests(TestCase):
             name='Cucumber'
         )
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            name='Steak and Mushroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.name)
